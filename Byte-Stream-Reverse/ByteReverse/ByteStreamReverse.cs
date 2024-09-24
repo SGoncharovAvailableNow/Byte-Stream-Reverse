17bd1cad07a3e03be0829989c28916e40199fb23
Serg_Goncharov <s.goncharov@sh.dunice.net>
2024-09-24 09:51:28 +0300

    Finished

---
+++ Byte-Stream-Reverse/ByteReverse/ByteStreamReverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte_Stream_Reverse.ByteReverse
+{
+    internal class ByteStreamReverse
+    {
+        public byte[] ReverseByte(byte[] bytes) 
+        {
+            var result = new List<byte>();
+
+            for (int i = 0, y = 8; i < bytes.Length / 8; i++,y += 8) 
+            {
+                result.AddRange(bytes.ToList().GetRange(bytes.Length - y,8));
+            }
+
+            return result.ToArray();
+        }
+
+        public void ShowArray(byte[] bytes) 
+        {
+            foreach (byte b in bytes) 
+            {
+                Console.Write($" {b} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Byte-Stream-Reverse/Program.cs
+++ Byte-Stream-Reverse/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Byte_Stream_Reverse.ByteReverse;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ByteStreamReverse reverse = new ByteStreamReverse();
+
+        byte[] bytes = { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 };
+
+        reverse.ShowArray(bytes);
+
+        reverse.ShowArray(reverse.ReverseByte(bytes));
+
+    }
+}
